2720232e9e3464dc8d75d319837d9c71f300e78e
Rayen Graf <100278450+RayenGraf@users.noreply.github.com>
2024-09-08 22:41:56 +0200

    add autoplay on load and helper function

--- card2brain-audio-autoplay.js
+++ card2brain-audio-autoplay.js
@@ -10,16 +10,91 @@
 // @grant        unsafeWindow
 // ==/UserScript==
 
+/**
+ * Class Card2Brain_LearnSectionFixes
+ * @description fixes the autoplay + audio buttons in the learning section
+ * @return {void}
+ */
+class Card2Brain_LearnSectionFixes{
+    static pluginName;
+    static learnSetID;
+    static reversed;
+
+    static learnCard;
+    static primaryAudioButton;
+    static secondaryAudioButton;
+
+    /**
+     * constructor
+     * @description consstructor
+     * @return {void}
+     */
+    constructor() {
+        this.pluginName = "card2brain-audio-autoplay";
+        this.reversed = false;
+
+        // get the learnSetID and check if it founded
+        this.setLearnSetID(); 
+        if(!this.learnSetID){
+            console.error(`${this.pluginName}: learnSetID not found`);
+            return;
+        }
+
+        this.checkLearnCardReversed();
+        this.getLeanCardElements();
+        this.autoplay();
+    }
+
+    /**
+     * setLearnSetID
+     * @description set the learnSetID
+     * @return {void}
+     */
+    setLearnSetID(){
+        const url = window.location.href;
+        const match = url.match(/\/(\d{8}[^\/]*)\/?/);
+        if (match)  this.learnSetID = match[1]; 
+    }
+
+    /**
+     * checkLearnCardReversed
+     * @description checks if the learnCard is reversed
+     * @return {void}
+     */
+    checkLearnCardReversed(){
+        const url = window.location.href;
+        if(url.includes("reversed")) this.reversed = true;
+    }
+
+    /**
+     * getLeanCardElements
+     * @description gets the new elements
+     * @return {void}
+     */
+    getLeanCardElements(){
+        this.learnCard = $(".learn-card");
+        this.primaryAudioButton = this.learnCard.find(".link[onclick^=speak]")
+        this.secondaryAudioButton = null;
+    }
+
+    /**
+     * autoplay
+     * @description plays the audio directly in the learning section
+     * @return {void}
+     */
+    autoplay(){
+        this.primaryAudioButton.click();
+    }
+}
+
+/**
+ * INIT
+ * @description loads the plugin
+ * @return {void}
+ */
 unsafeWindow.onload = function () {
     setTimeout(function (){
-        initCard2BrainAudio2Test();
-        console.log("card2brain-audio-autoplay plugin is loaded");
+        new Card2Brain_LearnSectionFixes();
+        console.log(`${this.pluginName} plugin is loaded`);
     }, 100);
-
-
-    function initCard2BrainAudio2Test(){
-        let learnCard = $(".learn-card");
-        let audiobutton = learnCard.find(".link[onclick^=speak]")
-        audiobutton.click();
-    }
 };
