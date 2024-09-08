be450616c3957204394e5156bcc7870694c5ca62
Rayen Graf <100278450+RayenGraf@users.noreply.github.com>
2024-09-09 00:22:51 +0200

    add function => get all card informations from Card2Brain

--- card2brain-audio-autoplay.js
+++ card2brain-audio-autoplay.js
@@ -17,7 +17,9 @@
  */
 class Card2Brain_LearnSectionFixes{
     static pluginName;
+
     static learnSetID;
+    static learnCards;
     static reversed;
 
     static learnCard;
@@ -30,19 +32,62 @@ class Card2Brain_LearnSectionFixes{
      * @return {void}
      */
     constructor() {
+        // plugin details
         this.pluginName = "card2brain-audio-autoplay";
-        this.reversed = false;
 
-        // get the learnSetID and check if it founded
-        this.setLearnSetID(); 
-        if(!this.learnSetID){
-            console.error(`${this.pluginName}: learnSetID not found`);
-            return;
+        //=========================================================
+        // Card2Brain Informations 
+        //=========================================================
+            // get the learnSetID and check if it founded
+            this.setLearnSetID(); 
+            if(!this.learnSetID){
+                console.error(`${this.pluginName}: learnSetID not found`);
+                return;
+            }
+
+            // check if the learnCard is reversed
+            this.reversed = false;
+            this.checkLearnCardReversed();
+
+            // get the learnCards
+            this.learnCards = [];
+            this.getlearnCardsAll();
+        //=========================================================
+        // Fix Card2Brain Behaviour
+        //=========================================================
+            this.getLearnCardElements();
+            this.autoplay();
+        //=========================================================
+        console.log(`${this.pluginName} plugin is loaded`);
+    }
+
+
+    /**
+     * getlearnCardsAll
+     * @description gets the learnCards and their informations
+     * @return {void}
+     */
+    getlearnCardsAll(){
+        let learnCards = [];
+        let cardsCount = 305;
+        for(let i = 0; i < cardsCount-1; i++){
+            let url = `https://card2brain.ch/box/${this.learnSetID}/loadNextSlide?chapter=&offset=${i}`
+            $.get(url, function(data){
+                data = $(data);
+                let frontText = data.find(".flip-card-front section.fs-card").html().trim();
+                let backText = data.find(".flip-card-back section.fs-card p").html().trim();
+                let firstSpeakFunction = data.find(".link[onclick^=speak]:first").attr("onclick");
+                let secondSpeakFunction = data.find(".link[onclick^=speak]:last").attr("onclick");
+                let CardInfo = {
+                    frontText,
+                    backText,
+                    firstSpeakFunction,
+                    secondSpeakFunction
+                };
+                learnCards.push(CardInfo);
+                this.learnCards = learnCards;
+            });
         }
-
-        this.checkLearnCardReversed();
-        this.getLeanCardElements();
-        this.autoplay();
     }
 
     /**
@@ -67,11 +112,11 @@ class Card2Brain_LearnSectionFixes{
     }
 
     /**
-     * getLeanCardElements
+     * getLearnCardElements
      * @description gets the new elements
      * @return {void}
      */
-    getLeanCardElements(){
+    getLearnCardElements(){
         this.learnCard = $(".learn-card");
         this.primaryAudioButton = this.learnCard.find(".link[onclick^=speak]")
         this.secondaryAudioButton = null;
@@ -95,6 +140,5 @@ class Card2Brain_LearnSectionFixes{
 unsafeWindow.onload = function () {
     setTimeout(function (){
         new Card2Brain_LearnSectionFixes();
-        console.log(`${this.pluginName} plugin is loaded`);
     }, 100);
 };
